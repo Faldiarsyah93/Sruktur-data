0ba9f7b8285cebe320a101a7c243bdf04c929ea6
Faldiarsyah93 <117282648+Faldiarsyah93@users.noreply.github.com>
2022-11-20 18:15:45 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,33 +1,20 @@
-import java.util.Scanner;
-
-public class array02 {
-
-    public static void main(String[]args) {
-        Scanner sc = new Scanner(System.in);
-        int[] AbsGanjil = new int[10];
-        int[] AbsGenap = new int[10];
-        int gnp=0,gjl=0;
-
-        for(int i=1;i<=20;i++) {
-            if(i%2==1)	{
-                AbsGanjil[gjl]=i;
-                gjl++;
-            }
-            else {
-                AbsGenap[gnp]=i;
-                gnp++;
+public class array2 {
+    public static void main(String[] args) {
+        System.out.println("Faldiarsyah Dwiki Permana");
+        System.out.println("UTS");int [] bil = {2, 4, 10, 11, 13, 16, 19, 200,112,116,119, 45, 23, 89, 111};
+        System.out.print("Bilangan Genap  :  ");
+        for (int i = 0; i < bil.length; i++){
+            if (bil [i] % 2 == 0) {
+                System.out.print(bil[i] + " ");
             }
         }
-
-        System.out.println("Data Absen Ganjil");
-        for(int i=0;i<10;i++) {
-            System.out.println("Ganjil ke "+i+"="+AbsGanjil[i]);
+        System.out.println("");
+        System.out.print("Bilangan Ganjil :  ");
+        for (int i = 0; i < bil.length; i++){
+            if (bil [i] % 2!= 0){
+                System.out.print(bil[i] + " ");
+            }
         }
-
-        System.out.println("Data Absen Genap");
-        for(int i=0;i<10;i++) {
-            System.out.println("Genap ke "+i+"="+AbsGenap[i]);
-        }
+        System.out.println("");
     }
-
 }
